b120f7665b75c48d1dd83e82fd38bc104cfc293d
ahmet baygın <amett1991@windowslive.com>
2022-01-12 20:29:04 +0300

    dewam

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product eklendi";
+        public static string ProductNameInvalid = "Product ismi geçersiz";
+        public static string MaintenanceTime="bakımdayız ";
+        public static string ProductsListed="ürünler listelendi";
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //readonly yani sadece get varsa constructor 'ın içinde değer verilebilir başka yerde değer atanamaz
+        public Result(bool success,string message):this(success)
+        //this derken bulundugu classtan bahsediyor parantez içinde tek parametre olduğu için diğer ctor çalışır ve kod tekrarı olmaz
+        {
+            Message = message;
+            
+
+        }
+        public Result(bool success)
+        {
+            Success = success;
+
+        }
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
